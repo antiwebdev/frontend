5b95969373ce50e02c0d8cc1c7d4f1a5b3f03e48
Andsu <antipiskun@gmail.com>
2023-12-07 19:28:36 +0200

    menu and carusel working together

--- src/containers/Circle/CircleMenu.tsx
+++ src/containers/Circle/CircleMenu.tsx
@@ -1,81 +1,76 @@
-import React, { useState, useEffect } from "react";
+import React, {Dispatch, SetStateAction,useState, useEffect}from "react";
 import MenuItem from "./MenuItem";
 import * as Styled from "./CircleMenu.styles";
+import { TDateItem } from "../Layout/Layout";
+import {dateItems} from "../Layout/Layout";
 
 
 interface CircleMenuProps {
     activeButton: number;
     setActiveButton: (buttonId: number) => void;
+    setState: Dispatch<SetStateAction<TDateItem>>;
 }
 
 interface MenuItemType {
     position: { x: number; y: number };
     buttonText: string;
     buttonId: number;
+    categoryId: number;
 }
 
-const CircleMenu: React.FC<CircleMenuProps> = ({ activeButton, setActiveButton }) => {
+const CircleMenu: React.FC<CircleMenuProps> = ({ setState, activeButton, setActiveButton }) => {
     const [menuItems, setMenuItems] = useState<MenuItemType[]>([]);
-    const years = [
-        {id: 1, leftYear: 1900, rightYear: 1925},
-        {id: 2, leftYear: 1925, rightYear: 1950},
-        {id: 3, leftYear: 1950, rightYear: 1975},
-        {id: 4, leftYear: 1975, rightYear: 2000},
-        {id: 5, leftYear: 2000, rightYear: 2025},
-        {id: 6, leftYear: 2025, rightYear: 2050},
-    ]
 
-    const items: MenuItemType[] = [
-        { buttonText: "Наука", buttonId: 1, position: { x: 0, y: 0 } },
-        { buttonText: "Игры", buttonId: 2, position: { x: 0, y: 0 } },
-        { buttonText: "История", buttonId: 3, position: { x: 0, y: 0 } },
-        { buttonText: "Кино", buttonId: 4, position: { x: 0, y: 0 } },
-        { buttonText: "Театры", buttonId: 5, position: { x: 0, y: 0 } },
-        { buttonText: "Медицина", buttonId: 6, position: { x: 0, y: 0 } },
-    ];
-
-    const filteredArr = years.filter((obj) => obj.id === activeButton);
+    const currentMenuItem = dateItems.find((obj) => obj.id === activeButton);
 
     useEffect(() => {
-        
-        const length = items.length;
-        const arc = 2 * Math.PI * (1 / length);
+        if (!currentMenuItem) return;
+
+        const length = dateItems.length;
+        const arc = (2 * Math.PI) / length;
         const radius = 50;
 
-        const positionedItems = items.map((item, i) => {
+        const positionedItems = dateItems.map((item, i) => {
             const angle = i * arc;
             const x = radius * Math.cos(angle);
             const y = radius * Math.sin(angle);
-            return { ...item, position: { x, y } };
+            return { buttonText: item.name, buttonId: item.id, position: { x, y }, categoryId: currentMenuItem.id };
         });
 
         setMenuItems(positionedItems);
-    }, []);
+    }, [currentMenuItem, activeButton]);
+
+    const handleClick = (itemId: number) => {
+        setActiveButton(itemId);
+        const selectedItem = dateItems.find((item) => item.id === itemId);
+        if (selectedItem) {
+            setState(selectedItem);
+        }
+    };
 
     return (
         <Styled.Circle>
-            <Styled.CenterHeight/>
-            <Styled.CenterWidth/>
+            <Styled.CenterHeight />
+            <Styled.CenterWidth />
             <Styled.CircularMenu>
-                {filteredArr.map((obj) => (
+                {currentMenuItem && (
                     <Styled.Years>
-                        <b style={{color: "#5D5FEF"}}>{obj.leftYear}</b>
-                        <b style={{color: "#EF5DA8"}}>{obj.rightYear}</b>
+                        <b style={{ color: "#5D5FEF" }}>{currentMenuItem.startDate}</b>
+                        <b style={{ color: "#EF5DA8" }}>{currentMenuItem.endDate}</b>
                     </Styled.Years>
-                ))}
+                )}
                 {menuItems.map((item) => (
                     <MenuItem
                         key={item.buttonId}
                         buttonText={item.buttonText}
                         buttonId={item.buttonId}
                         isActive={activeButton === item.buttonId}
-                        onClick={() => setActiveButton(item.buttonId)}
+                        onClick={() => handleClick(item.buttonId)}
                         position={item.position}
                     />
                 ))}
             </Styled.CircularMenu>
         </Styled.Circle>
-       
     );
 };
 
--- src/containers/Layout/Layout.tsx
+++ src/containers/Layout/Layout.tsx
@@ -10,7 +10,7 @@ export interface  IItems {
     description: string
 }
 
-interface TDateItem {
+export interface TDateItem {
     id: number
     name: string
     startDate: string
@@ -18,7 +18,7 @@ interface TDateItem {
     items: Array<IItems>
 }
 
-const dateItems: Array<TDateItem> = [
+export const dateItems: Array<TDateItem> = [
     {
         id: 1,
         name: 'Музыка',
@@ -28,7 +28,22 @@ const dateItems: Array<TDateItem> = [
             {
                 id: 1,
                 date: '1982',
-                description: 'music1'
+                description: 'Выход альбома "Thriller" Майкла Джексона'
+            },
+            {
+                id: 2,
+                date: '1983',
+                description: 'Концерт "Live Aid" для борьбы с голодом в Эфиопии'
+            },
+            {
+                id: 3,
+                date: '1984',
+                description: 'Выход альбома "Purple Rain" Принса'
+            },
+            {
+                id: 4,
+                date: '1985',
+                description: 'Группа Queen выступает на стадионе Уэмбли'
             }
         ]
     },
@@ -36,7 +51,7 @@ const dateItems: Array<TDateItem> = [
         id: 2,
         name: 'Кино',
         startDate: '1987',
-        endDate: '1991',
+        endDate: '1990',
         items: [
             {
                 id: 1,
@@ -59,17 +74,129 @@ const dateItems: Array<TDateItem> = [
                 description: 'Крепкий орешек/Die hard, США'
             }
         ]
+    },
+    {
+        id: 3,
+        name: 'Литература',
+        startDate: '1992',
+        endDate: '1996',
+        items: [
+            {
+                id: 1,
+                date: '1992',
+                description: 'Роман "Преступление и наказание", Ф.М. Достоевский'
+            },
+            {
+                id: 2,
+                date: '1993',
+                description: 'Поэма "Унесённые ветром", М. Митчелл'
+            },
+            {
+                id: 3,
+                date: '1994',
+                description: 'Рассказ "Шум и ярость", У. Фолкнер'
+            },
+            {
+                id: 4,
+                date: '1995',
+                description: 'Роман "Три товарища", Э.М. Ремарк'
+            }
+        ],
+    },
+    {
+        id: 4,
+        name: 'Наука',
+        startDate: '1997',
+        endDate: '2001',
+        items: [
+            {
+                id: 1,
+                date: '1997',
+                description: 'Первый успешный клонированный млекопитающий, овца Долли'
+            },
+            {
+                id: 2,
+                date: '1999',
+                description: 'Международная космическая станция начинает свою работу'
+            },
+            {
+                id: 3,
+                date: '2000',
+                description: 'Расшифрована человеческая геномная последовательность'
+            },
+            {
+                id: 4,
+                date: '2001',
+                description: 'Первый полет частного космического корабля, космический корабль Союз'
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: 'Искусство',
+        startDate: '2002',
+        endDate: '2006',
+        items: [
+            {
+                id: 1,
+                date: '2002',
+                description: 'Открытие Музея современного искусства в Нью-Йорке'
+            },
+            {
+                id: 2,
+                date: '2003',
+                description: 'Выход фильма "Пианист", режиссер Р. Полански'
+            },
+            {
+                id: 3,
+                date: '2004',
+                description: 'Открытие Лувра в Абу-Даби'
+            },
+            {
+                id: 4,
+                date: '2006',
+                description: 'Выход фильма "Исчезнувшая" (The Departed), режиссер М. Скорсезе'
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: 'Технологии',
+        startDate: '2007',
+        endDate: '2011',
+        items: [
+            {
+                id: 1,
+                date: '2007',
+                description: 'Запуск первого iPhone'
+            },
+            {
+                id: 2,
+                date: '2008',
+                description: 'Запуск Google Chrome'
+            },
+            {
+                id: 3,
+                date: '2010',
+                description: 'Выход первого iPad'
+            },
+            {
+                id: 4,
+                date: '2011',
+                description: 'Запуск социальной сети Instagram'
+            }
+        ]
     }
 ]
 
 const Layout: FC  = () => {
 
-    const [state, setState] = useState<TDateItem>(dateItems[1])
+    const [state, setState] = useState<TDateItem>(dateItems[0])
     const [activeButton, setActiveButton] = useState(1);
 
     return(
         <Styled.Container>
-            <CircleMenu activeButton={activeButton} setActiveButton={setActiveButton}/>
+            <CircleMenu setState={setState} activeButton={activeButton} setActiveButton={setActiveButton}/>
             <Styled.Inner>    
                 <Carousel items={state?.items}/>
             </Styled.Inner>
@@ -77,4 +204,4 @@ const Layout: FC  = () => {
     )
 }
 
-export default Layout
+export default Layout;
